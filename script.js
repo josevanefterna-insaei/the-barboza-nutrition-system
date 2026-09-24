61c7e5bddcb4577f0e36265f4909ea4588daa99a
josevanefterna-insaei <josevanefterna@gmail.com>
2026-09-24 19:18:01 -0300

    Add R$37 launch checkout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const CHECKOUT_URL = "COLE_AQUI_O_LINK_DO_CHECKOUT_R37";
+const CHECKOUT_URL = "https://pay.hotmart.com/W107707314T?off=l4fqztts";
 document.querySelectorAll(".checkout").forEach(a=>a.addEventListener("click",e=>{
  if(CHECKOUT_URL.startsWith("COLE_")){e.preventDefault();alert("Falta apenas inserir no script.js o link exato do checkout de lançamento de R$37.");}
  else a.href=CHECKOUT_URL;
